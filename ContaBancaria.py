f12edbf0b921f73896920633b1df208f1afa5f4f
aluno7 <lennedys@yahoo.com.br>
2026-07-20 18:01:07 -0300

    respostas

--- ContaBancaria.py
+++ ContaBancaria.py
@@ -3,36 +3,38 @@ from tkinter import messagebox, simpledialog
 from Classes import Cliente, ContaBancaria, Endereço, ContaCorrente
 
 class BancoApp:
-def __init__(self, janela):
-self.janela = janela
+  def __init__(self, janela): self.janela = janela
 self.janela.title("Caixa economico - POO em Python")
 self.janela.geometry("850x400")
 self.janela.configure(background="#a4d16d")
+
 endereço1 = Endereço("Rua das Flores", 25, "Centro", "Natal")
 endereço2 = Endereço("Avenida Brasil", 100, "Jardim América", "Recife")
 cliente1 = Cliente("Carlos", "123.456.789-00", endereço1)
 cliente2 = Cliente("Mariana", "987.654.321-00", endereço2)
+
 self.contas = [
 ContaCorrente(cliente1, 1001, 500, 100, 10),
 ContaBancaria(cliente2, 1002, 1000)
 ]
 self.criar_interface()
 def criar_interface(self):
-titulo = tk.Label(
+ titulo = tk.Label(
 self.janela,
 text="Caixa economico - Contas Bancárias",
 font=("Arial", 18, "bold"),
 background="#cbf09e"
 )
-titulo.pack(pady=15)
-self.frame_contas = tk.Frame(self.janela)
-self.frame_contas.pack()
-self.atualizar_tela()
+ 
+ titulo.pack(pady=15)
+ self.frame_contas = tk.Frame(self.janela)
+ self.frame_contas.pack()
+ self.atualizar_tela()
 def atualizar_tela(self):
-for widget in self.frame_contas.winfo_children():
-widget.destroy()
+ for widget in self.frame_contas.winfo_children():
+  widget.destroy()
 for conta in self.contas:
-frame = tk.Frame(
+ frame = tk.Frame(
 self.frame_contas,
 borderwidth=2,
 
@@ -48,12 +50,14 @@ text=conta.get_titular(),
 font=("Arial", 14, "bold"),
 background="#cbf09e"
 )
+
 lbl_titular.pack()
 lbl_numero = tk.Label(
 frame,
 text=f"Conta: {conta.get_numero()}",
 background="#cbf09e"
 )
+
 lbl_numero.pack()
 lbl_saldo = tk.Label(
 frame,
@@ -61,6 +65,7 @@ text=f"Saldo: R$ {conta.get_saldo():.2f}",
 font=("Arial", 12),
 background="#cbf09e"
 )
+
 lbl_saldo.pack(pady=5)
 btn_depositar = tk.Button(
 frame,
@@ -69,6 +74,7 @@ width=15,
 background="#a4d16d",
 command=lambda c=conta: self.depositar(c)
 )
+
 btn_depositar.pack(pady=2)
 btn_sacar = tk.Button(
 frame,
---
+++ implementaçoes.py
@@ -0,0 +1,26 @@
+cliente
+def quantidades_contas(self):
+    return len (self. __contas)
+ def consultar_saldo_total(self):
+    total = 0
+    for conta in self. __contas:
+        total += conta.get_saldo()
+
+
+        2
+        def pix(self,valor conta_destino_):
+            if self. sacar(valor):
+                conta_destino.depositar(valor)
+                return True
+            return False
+        
+        3
+        class
+            containvestimento(contabancaria):
+            def__init__(self,cliente,numero,saldo
+                        taxa de rendimento, taxa de administração)
+            super().__init__(cliente,numero,saldo)
+            self.__taxa.rendimento = taxa_rendimento
+            self.taxa__. administração = taxa_administração
+            def get_tipo.conta
+        
